d0c83c74178c84c30ca55cd196d58cc903c3a72e
hashmup <hashmup@gmail.com>
2017-03-30 16:43:24 +0900

    done demo app

--- app/src/main/java/com/hashmup/demoapp/MainActivity.java
+++ app/src/main/java/com/hashmup/demoapp/MainActivity.java
@@ -1,13 +1,74 @@
 package com.hashmup.demoapp;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int cur = 0;
+    private String[] nums = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+    private Button[] buttons = {};
+    long tStart = 0, tEnd = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setButton();
+    }
+
+    private void setButton() {
+        Collections.shuffle(Arrays.asList(nums));
+        Button startBtn = (Button)findViewById(R.id.startBtn);
+        startBtn.setVisibility(View.GONE);
+        for (int i=0;i<9;i++) {
+            String id = "button" + Integer.toString(i+1);
+            int resId = getResources().getIdentifier(id, "id", getPackageName());
+            Button btn = (Button)findViewById(resId);
+            btn.setBackgroundColor(Color.LTGRAY);
+            btn.setText(nums[i]);
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String tappedNum = ((Button)v).getText().toString();
+                    if (cur == 0 && tStart == 0) {
+                        tStart = System.currentTimeMillis();
+                    }
+                    if (tappedNum == Integer.toString(cur+1)) {
+                        v.setBackgroundColor(Color.YELLOW);
+                        cur++;
+                    }
+                    if (cur == 9) {
+                        tEnd = System.currentTimeMillis();
+                        showTime();
+                    }
+                    System.out.println(((Button)v).getText().toString());
+                }
+            });
+        }
+    }
+
+    private void showTime() {
+        final TextView tv = (TextView)findViewById(R.id.textView);
+        Button btn = (Button)findViewById(R.id.startBtn);
+
+        long tDelta = tEnd - tStart;
+        double elapsedSeconds = tDelta / 1000.0;
+        tv.setText(Double.toString(elapsedSeconds));
+        btn.setVisibility(View.VISIBLE);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setText("");
+                setButton();
+                cur = 0;
+                tStart = 0;
+            }
+        });
     }
 }
